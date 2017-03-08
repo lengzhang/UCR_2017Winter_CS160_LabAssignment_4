4944c15a6898a375e0e9b7973be0aef893f5d57e
lengzhang <lengzhang@live.com>
2017-03-08 06:54:25 +0000

    replace problem_6.c by the problem template

--- problem_6.c
+++ problem_6.c
@@ -1,23 +1,31 @@
-#include <omp.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-int main (int argc, char *argv[])
+#define MAX_THREADS 4
+static long num_steps = 100000000;
+double step;
+int main ()
 {
-    int nthreads, tid;
-    
-    /* Fork a team of threads giving them their own copies of variables */
-    #pragma omp parallel private(nthreads, tid)
-    {
-        /* Obtain thread number */
-        tid = omp_get_thread_num();
-        printf("Hello World from thread = %d\n", tid);
-        
-        /* Only master thread does this */
-        if (tid == 0) 
+    int i, j;
+    double pi, full_sum = 0.0;
+    double start_time, run_time, x;
+    step = 1.0/(double) num_steps;
+    for(j=1; j<=MAX_THREADS; j++){
+        omp_set_num_threads(j);
+        full_sum = 0.0;
+        start_time = omp_get_wtime();
+        #pragma omp parallel
         {
-            nthreads = omp_get_num_threads();
-            printf("Number of threads = %d\n", nthreads);
+            int id = omp_get_thread_num();
+            int numthreads = omp_get_num_threads();
+            double partial_sum = 0;
+            for (i=id; i< num_steps; i+=numthreads){
+            x = (i+0.5)*step;
+            partial_sum = partial_sum + 4.0/(1.0+x*x);
         }
-    } /* All threads join master thread and disband */
+        full_sum += partial_sum;
+    }
+    pi = step * full_sum;
+    run_time = omp_get_wtime() - start_time;
+    printf("\n pi is %f in %f seconds %d threads \n ", pi,
+    run_time, j);
+    }
 }
