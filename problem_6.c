ba540b399a584e8e98edf8b3265f8f5b11299768
lengzhang <lengzhang@live.com>
2017-03-08 08:19:54 +0000

    fix bug and add answer

--- problem_6.c
+++ problem_6.c
@@ -40,7 +40,16 @@
 
 /*
     Answer:
+        In the parallel region of the program
+        Shared:     num_steps   step    i   full_sum    x
+        Private:    id  numthreads  partial_sum
         
+        Since the variable i is a shared variable, each thread will access the
+        same variable i. This causes race problem, so the final result pi will
+        not approximate to 3.14 when more than 1 thread in parallel. For this
+        problem, seting the variable i to be private can avoid race problem
+        because each thread has own copy of i. Therefore, the variable i of
+        thread A cannot be accessed by other thread.
 */
 #include <stdio.h>
 #define MAX_THREADS 4
@@ -57,11 +66,12 @@ int main ()
         omp_set_num_threads(j);
         full_sum = 0.0;
         start_time = omp_get_wtime();
-        #pragma omp parallel
+        #pragma omp parallel private(i)
         {
             int id = omp_get_thread_num();
             int numthreads = omp_get_num_threads();
             double partial_sum = 0;
+            
             for (i=id; i< num_steps; i+=numthreads)
             {
                 x = (i+0.5)*step;
@@ -71,7 +81,6 @@ int main ()
         }
         pi = step * full_sum;
         run_time = omp_get_wtime() - start_time;
-        printf("\n pi is %f in %f seconds %d threads \n ", pi,
-        run_time, j);
+        printf("\npi is %f in %f seconds %d threads\n", pi, run_time, j);
     }
 }
